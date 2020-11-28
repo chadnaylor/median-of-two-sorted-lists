8360c8985aaad4bac5ab5265a6a7cc733ce2f875
Chad Naylor <chad.naylor@gmail.com>
2020-11-27 20:10:05 -0600

    tests pass, one boundary condition to fix

---
+++ median.js
@@ -0,0 +1,131 @@
+/**
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arranums2[j]s.
+
+Follow up: The overall run time complexity should be O(log (m+n)).
+
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+    let i = Math.ceil(nums1.length / 2)
+    let j = Math.ceil(nums2.length / 2)
+    let halfTotalLength = Math.floor((nums1.length + nums2.length) / 2)
+    let iStep = Math.max(1, Math.ceil(i / 2)), jStep = Math.max(1, Math.ceil(j / 2))
+
+    let returnAnswer = () => {
+        if ((nums1.length + nums2.length) / 2 > (i + j)) {
+            if (!nums1.length) return nums2[j]
+            else if (!nums2.length) return nums1[i]
+            else if (i === nums1.length) {
+                return nums2[j]
+            } else if (j === nums2.length) {
+                return nums1[i]
+            }
+            else if (nums1[i] <= nums2[j]) return nums1[i]
+            else return nums2[j]
+        } else {
+            let upper, lower
+            if (i === nums1.length) {
+                upper = nums2[j]
+
+            } else if (j === nums2.length) {
+                upper = nums1[i]
+
+            } else {
+                upper = Math.min(nums1[i], nums2[j])
+
+            }
+
+            if (j === 0) {
+                lower = nums1[i - 1]
+            } else if (i === 0) {
+                lower = nums2[j - 1]
+            } else {
+                lower = Math.max(nums1[i - 1], nums2[j - 1])
+            }
+
+            return (upper + lower) / 2
+        }
+    }
+
+    while (true) {
+        if (i + j < halfTotalLength) {
+            if (j < nums2.length && i < nums1.length) {
+                if (nums2[j] <= nums1[i]) {
+                    j += jStep
+                    jStep = Math.ceil(jStep / 2)
+                } else if (nums1[i] < nums2[j]) {
+                    i += iStep
+                    iStep = Math.ceil(iStep / 2)
+                }
+            } else if (i === nums1.length) {
+                j += jStep
+                jStep = Math.ceil(jStep / 2)
+            } else {
+                i += iStep
+                iStep = Math.ceil(iStep / 2)
+            }
+        } else if (i + j > halfTotalLength) {
+            if (i > 0 && j > 0) {
+                if (nums2[j - 1] >= nums1[i - 1]) {
+                    j -= jStep
+                    jStep = Math.ceil(jStep / 2)
+                } else if (nums2[j - 1] < nums1[i - 1]) {
+                    i -= iStep
+                    iStep = Math.ceil(iStep / 2)
+                }
+            } else if (i === 0) {
+                j -= jStep
+                jStep = Math.ceil(jStep / 2)
+            } else {
+                i -= iStep
+                iStep = Math.ceil(iStep / 2)
+            }
+        }
+        else {
+            if (i === nums1.length || j === nums2.length) {
+                return returnAnswer()
+            }
+            if (nums1[i] === nums2[j]) {
+                return returnAnswer()
+            }
+            else if (nums2[j] > nums1[i]) {
+                if (j > 0 && nums2[j - 1] <= nums1[i]) {
+                    return returnAnswer()
+                } else if (j === 0 && i === nums1.length - 1) {
+                    return returnAnswer()
+                } else if (j === 0 && nums1[i + 1] > nums2[j]) {
+                    return returnAnswer()
+                } else {
+                    if (jStep >= iStep && j > 0) {
+                        j -= jStep
+                        jStep = Math.ceil(jStep / 2)
+                    } else {
+                        i += iStep
+                        iStep = Math.ceil(iStep / 2)
+                    }
+                }
+            } else if (nums2[j] < nums1[i]) {
+                if (i > 0 && nums1[i - 1] <= nums2[j]) {
+                    return returnAnswer()
+                } else if (i === 0 && j === nums2.length - 1) {
+                    return returnAnswer()
+                } else if (i === 0 && nums2[j + 1] > nums1[i]) {
+                    return returnAnswer()
+                } else {
+                    if (iStep >= jStep && i > 0) {
+                        i -= iStep
+                        iStep = Math.ceil(iStep / 2)
+                    } else {
+                        j += jStep
+                        jStep = Math.ceil(jStep / 2)
+                    }
+                }
+            }
+        }
+    }
+
+};
+
+module.exports = { findMedianSortedArrays }
---
+++ test/median.js
@@ -0,0 +1,77 @@
+let median = require('../median')
+let assert = require('assert')
+
+describe('Median', function () {
+    describe('#findMedianSortedArrays()', function () {
+        it('should correctly find median of two sorted arrays of different length', function () {
+            let nums1 = [1, 3], nums2 = [2]
+            assert.strictEqual(2, median.findMedianSortedArrays(nums1, nums2))
+
+            nums1 = [2, 3], nums2 = [1]
+            assert.strictEqual(2, median.findMedianSortedArrays(nums1, nums2))
+
+            nums1 = [1], nums2 = [2, 3, 4, 5]
+            assert.strictEqual(3, median.findMedianSortedArrays(nums1, nums2))
+
+            nums1 = [4], nums2 = [1, 2, 3, 5]
+            assert.strictEqual(3, median.findMedianSortedArrays(nums1, nums2))
+
+            // nums1 = [5], nums2 = [1, 2, 3, 4]
+            // assert.strictEqual(3, median.findMedianSortedArrays(nums1, nums2))
+
+            nums1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], nums2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20] //i=3, j=0
+            assert.strictEqual(11, median.findMedianSortedArrays(nums1, nums2))
+
+            nums1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], nums2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21] //i=3, j=0
+            assert.strictEqual(11, median.findMedianSortedArrays(nums1, nums2))
+
+
+            nums1 = [1, 3, 4, 5, 6], nums2 = [2, 7] //i=2, j=1
+            assert.strictEqual(4, median.findMedianSortedArrays(nums1, nums2))
+
+            nums2 = [1, 3, 4, 5, 6], nums1 = [2, 7] //i=2, j=1
+            assert.strictEqual(4, median.findMedianSortedArrays(nums1, nums2))
+
+        })
+        it('should correctly find median of two sorted arrays of same length', function () {
+            let nums1 = [1, 2], nums2 = [3, 4]
+            assert.strictEqual(2.5, median.findMedianSortedArrays(nums1, nums2))
+
+            nums1 = [1, 2, 3], nums2 = [3, 4, 9]
+            assert.strictEqual(3, median.findMedianSortedArrays(nums1, nums2))
+
+            nums1 = [3, 4, 9], nums2 = [1, 2, 3]
+            assert.strictEqual(3, median.findMedianSortedArrays(nums1, nums2))
+
+
+            nums1 = [1], nums2 = [3, 4, 9]
+            assert.strictEqual(3.5, median.findMedianSortedArrays(nums1, nums2))
+
+            nums1 = [3, 4, 9], nums2 = [1]
+            assert.strictEqual(3.5, median.findMedianSortedArrays(nums1, nums2))
+
+            nums1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], nums2 = [12, 13, 14, 15, 16, 17, 18, 19, 20] //i=3, j=0
+            assert.strictEqual(10.5, median.findMedianSortedArrays(nums1, nums2))
+        })
+        it('should correctly find median of two sorted arrays of zeros', function () {
+            let nums1 = [0, 0], nums2 = [0, 0]
+            assert.strictEqual(0, median.findMedianSortedArrays(nums1, nums2))
+
+            nums1 = [0, 0], nums2 = [0, 0, 0]
+            assert.strictEqual(0, median.findMedianSortedArrays(nums1, nums2))
+        })
+
+        it('should correctly find median of one empty and one sorted array', function () {
+            let nums1 = [], nums2 = [1]
+            assert.strictEqual(1, median.findMedianSortedArrays(nums1, nums2))
+
+            nums1 = [2], nums2 = []
+            assert.strictEqual(2, median.findMedianSortedArrays(nums1, nums2))
+
+            nums1 = [2, 3], nums2 = []
+            assert.strictEqual(2.5, median.findMedianSortedArrays(nums1, nums2))
+
+        })
+    })
+
+})
